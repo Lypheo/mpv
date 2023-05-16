3a571fb376818255a7fc24cfa8ed10f060192d66
Lypheo <saifullah124@hotmail.de>
2023-05-16 02:09:08 +0200

    scripting: add second C plugin entrypoint for Windows support
    The previous unix-only C plugin system worked by leaving references to
    mpv symbols in the plugin unresolved at compile time and resolving them
    with the executable’s symbols at runtime. This did not work
    on Windows due to the explicit nature of the table of imported symbols.
    This commit adds a second entrypoint that takes a struct of all libmpv API
    functions as a parameter to work around the restrictions in PE and enable
    Windows support.
    
    Since the new entrypoint should be supported by all platforms,
    the compiler checks in the build system are dropped.
    If there is a non-win32 platform that doesn’t support -rdynamic,
    this might lead to issues, but who cares.

--- player/scripting.c
+++ player/scripting.c
@@ -41,6 +41,8 @@
 #include "client.h"
 #include "libmpv/client.h"
 
+extern const struct mpapi mpapi_internal;
+
 extern const struct mp_scripting mp_scripting_lua;
 extern const struct mp_scripting mp_scripting_cplugin;
 extern const struct mp_scripting mp_scripting_js;
@@ -298,21 +300,30 @@ bool mp_load_scripts(struct MPContext *mpctx)
 #include <dlfcn.h>
 
 #define MPV_DLOPEN_FN "mpv_open_cplugin"
+#define MPV_DLOPEN_FN2 "mpv_open_cplugin2"
 typedef int (*mpv_open_cplugin)(mpv_handle *handle);
+typedef int (*mpv_open_cplugin2)(mpv_handle *handle, const struct mpapi *mpapi);
 
 static int load_cplugin(struct mp_script_args *args)
 {
     void *lib = dlopen(args->filename, RTLD_NOW | RTLD_LOCAL);
     if (!lib)
         goto error;
+
+    char *err;
     // Note: once loaded, we never unload, as unloading the libraries linked to
     //       the plugin can cause random serious problems.
     mpv_open_cplugin sym = (mpv_open_cplugin)dlsym(lib, MPV_DLOPEN_FN);
-    if (!sym)
-        goto error;
+    if (!sym) {
+        err = dlerror();
+        mpv_open_cplugin2 sym2 = (mpv_open_cplugin2) dlsym(lib, MPV_DLOPEN_FN2);
+        if (!sym2)
+            goto error;
+        else
+            return sym2(args->client, &mpapi_internal) ? -1 : 0;
+    }
     return sym(args->client) ? -1 : 0;
-error: ;
-    char *err = dlerror();
+error:
     if (err)
         MP_ERR(args, "C plugin error: '%s'\n", err);
     return -1;
